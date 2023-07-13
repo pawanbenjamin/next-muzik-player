19a2ea575a23420600b93cc9ba3720d5d19531e7
pawanbenjamin <pawanbenjamin@gmail.com>
2023-07-13 17:22:44 -0500

    fetch playlist from server

--- app/player/playlist/[id]/page.tsx
+++ app/player/playlist/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { SongsTable } from "@/app/components/SongsTable";
+import useMe from "@/app/lib/hooks/useMe";
 import prisma from "@/app/lib/prisma";
 
 type Props = {
@@ -8,6 +9,7 @@ type Props = {
 };
 
 export default async function Playlist({ params }: Props) {
+  const user = useMe();
   const playlist = await prisma.playlist.findFirst({
     where: {
       id: +params.id
@@ -17,13 +19,19 @@ export default async function Playlist({ params }: Props) {
     }
   });
 
+  const playlists = await prisma.playlist.findMany({
+    where: {
+      userId: user?.id
+    }
+  });
+
   return (
     <div className="flex flex-col h-full">
       <div className="flex flex-col justify-end pl-2 pb-8 h-80">
         <h1>Playlist {playlist?.id}</h1>
       </div>
       <ul>
-        <SongsTable songs={playlist?.songs} />
+        <SongsTable songs={playlist?.songs} playlists={playlists} />
       </ul>
     </div>
   );
